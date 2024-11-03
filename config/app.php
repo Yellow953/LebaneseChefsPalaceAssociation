5090a41b362cd59cc08e3a53f9d425b4797d1a9a
Yellow <joemazloum953@gmail.com>
2024-11-03 12:58:40 +0200

    Lebanese Chef's Palace Association

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Costpitality'),
+    'name' => env('APP_NAME', "Lebanese Chef's Palace Association"),
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Costpitality</title>
+    <title>Lebanese Chef's Palace Association</title>
 </head>
 
 <body>
