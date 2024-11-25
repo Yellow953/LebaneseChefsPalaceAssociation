2978d7033dfe5f59f718a6946d8428d44fd4e8e2
EdwardBejjani <edwardbejjani@gmail.com>
2024-11-25 09:45:03 +0200

    layout error fixes

--- resources/views/frontend/layout/layout.blade.php
+++ resources/views/frontend/layout/layout.blade.php
@@ -60,8 +60,6 @@
     <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
     <script src="{{ asset('assets/plugins/custom/fslightbox/fslightbox.bundle.js') }}"></script>
     <script src="{{ asset('assets/plugins/custom/typedjs/typedjs.bundle.js') }}"></script>
-    <script src="{{ asset('assets/js/custom/landing.js') }}"></script>
-    <script src="{{ asset('assets/js/custom/pages/pricing/general.js') }}"></script>
 </body>
 
 </html>
